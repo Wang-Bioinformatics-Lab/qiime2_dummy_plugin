fab0bf7ba9eb244d3f7fde19498562cdfe494803
user <user@yunshu.cs.ucr.edu>
2023-10-09 18:15:12 -0700

    tested corectly

--- qiime2_dummy_plugin/dummy_functions.py
+++
@@ -1,8 +0,0 @@
-import qiime2
-from q2_types.feature_table import FeatureTable, RelativeFrequency
-
-def dummy_function(input_artifact: FeatureTable[RelativeFrequency]) -> FeatureTable[RelativeFrequency]:
-    print("dummy_function")
-    # Simply return the input artifact as-is
-    return input_artifact
-
